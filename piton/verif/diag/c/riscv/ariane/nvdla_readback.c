637dcca6a874fc02352619a39471d352e939ce2f
Tigar Cyr <tcyr@princeton.edu>
2020-01-11 18:44:45 -0500

    c tests cleanup

--- piton/verif/diag/c/riscv/ariane/nvdla_readback.c
+++ piton/verif/diag/c/riscv/ariane/nvdla_readback.c
@@ -2,10 +2,16 @@
 #include "../piton/verif/diag/c/riscv/ariane/nvdla_glb.h"
 #include "../piton/verif/diag/c/riscv/ariane/nvdla.h"
 
+// updates NVDLA interrupt mask and checks that change is reflected
 int main(int argc, char ** argv) {
-  
-  printf("INTR_MASK %08x\n", *(base_addr + GLB_INTR_MASK));
-  *(base_addr + GLB_INTR_MASK) += 1; 
-  printf("INTR_MASK %08x\n", *(base_addr + GLB_INTR_MASK));
+  unsigned int old = *(BASE_ADDR + GLB_INTR_MASK);
+  printf("INTR_MASK %08x\n", old);
+
+  *(BASE_ADDR + GLB_INTR_MASK) += 1;
+
+  unsigned int new = *(BASE_ADDR + GLB_INTR_MASK); 
+  printf("INTR_MASK %08x\n", new);
+
+  printf("TEST %sED\n", old + 1 == new ? "PASS" : "FAIL");
   return 0;
 }
