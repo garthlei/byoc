d273cc66aa3c171f8e5c85aac689e1a2a679fb75
Jonathan Balkind <jbalkind@princeton.edu>
2020-05-24 21:08:07 -0400

    Adding new devices_blackparrot.xml

--- piton/tools/bin/pyhplib.py
+++ piton/tools/bin/pyhplib.py
@@ -135,6 +135,9 @@ fileName = "devices.xml"
 if os.getenv("PITON_ARIANE") is not None:
     if int(os.getenv("PITON_ARIANE")):
         fileName = "devices_ariane.xml"
+if os.getenv("PITON_BLACKPARROT") is not None:
+    if int(os.getenv("PITON_BLACKPARROT")):
+        fileName = "devices_blackparrot.xml"
 
 DEVICES_XML_FILENAME = os.path.join(os.getenv("PROTOSYN_RUNTIME_DESIGN_PATH", ""),
                                     os.getenv("PROTOSYN_RUNTIME_BOARD", ""),
